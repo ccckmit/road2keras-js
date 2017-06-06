2894e14158e454f6d3e0725d171ea31882f6d623
ccckmit <ccckmit@gmail.com>
2017-06-06 08:36:37 +0800

    modularize 05-SentBiLSTM

--- 05-SentBiLSTM/SentiBiLSTM.js
+++ 05-SentBiLSTM/SentiBiLSTM.js
@@ -5,7 +5,9 @@
 const KerasJS = require('keras-js')
 const ops = require('ndarray-ops')
 const Tensor = KerasJS.Tensor
-const findIndex = require('lodash').findIndex
+const lodash = require('lodash')
+const findIndex = lodash.findIndex
+const random = lodash.random
 
 var self = {}
 
@@ -50,9 +52,9 @@ const START_WORD_INDEX = 1
 const OOV_WORD_INDEX = 2
 const INDEX_FROM = 3
 
-self.model.ready().then(() => {
+self.randomInput = function () {
   self.isSampleText = true
-  const randSampleIdx = 1 // random(0, self.testSamples.length - 1)
+  const randSampleIdx = random(0, self.testSamples.length - 1)
   const values = self.testSamples[randSampleIdx].values
   self.sampleTextLabel = self.testSamples[randSampleIdx].label === 0 ? 'negative' : 'positive'
   const words = values.map(idx => {
@@ -64,15 +66,47 @@ self.model.ready().then(() => {
       return self.wordDict[idx - INDEX_FROM]
     }
   })
-  self.inputText = words.join(' ').trim()
-  console.log('inputText=%s', self.inputText)
   self.inputTextParsed = words.filter(w => !!w)
+  self.inputText = words.join(' ').trim()
   self.input = new Float32Array(values)
+  return self.input
+}
+
+self.textInput = function (text) {
+  this.input = new Float32Array(MAXLEN)
+  // by convention, use 2 as OOV word
+  // reserve 'index_from' (=3 by default) characters: 0 (padding), 1 (start), 2 (OOV)
+  // see https://github.com/fchollet/keras/blob/master/keras/datasets/imdb.py
+  this.inputText = text
+  this.inputTextParsed = this.inputText.trim().toLowerCase().split(/[\s\.,!?]+/gi)  
+  let indices = this.inputTextParsed.map(word => {
+    const index = this.wordIndex[word]
+    return !index ? OOV_WORD_INDEX : index + INDEX_FROM
+  })
+  indices = [START_WORD_INDEX].concat(indices)
+  indices = indices.slice(-MAXLEN)
+  // padding and truncation (both pre sequence)
+  const start = Math.max(0, MAXLEN - indices.length)
+  for (let i = start; i < MAXLEN; i++) {
+    this.input[i] = indices[i - start]
+  }
+}
+
+self.predict = function () {
   self.model.predict({ input: self.input }).then(outputData => {
     self.output = outputData.output
     self.stepwiseCalc()
     console.log('stepwiseOutput=', self.stepwiseOutput)
     self.modelRunning = false
   })
-})
+}
 
+var testText =
+`murder over new york is fun but not as good as most of the other fox <OOV> this film would have been better named charlie chan in new york the film's working title this is chance to play chan in the big apple there is a lot to like here though including guest star shemp howard of the three stooges br br this has one of my favorite chan <OOV> coincidence like ancient egg leave unpleasant <OOV> and <OOV> are good in this one and so is the supporting cast but there is little or no mysterious atmosphere which i look for in these films still it is good to see`
+
+self.model.ready().then(() => {
+//  self.randomInput() // 隨機選取一個樣本中的 input
+  self.textInput(testText)
+  console.log('inputText=%s', self.inputText)
+  self.predict()
+})
